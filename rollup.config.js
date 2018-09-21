bb6ba78e04f539c1c760b79b6e1740360a7ac9dd
Florian Husquinet <florian.husquinet@deegital.Be>
2018-09-21 17:42:59 +0200

    Revert "✨ ES5 Compile"
    This reverts commit 82abae849738f558647a3df653af726fdaca8e4b.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
@@ -8,17 +7,13 @@ export default [
 	{
 		input: 'src/js/index.js',
 		output: {
-			exports: 'named',
 			name: 'Toasteo',
 			file: pkg.browser,
 			format: 'umd'
 		},
 		plugins: [
 			resolve(), // so Rollup can find `ms`
-			commonjs(), // so Rollup can convert `ms` to an ES module
-			babel({
-				exclude: ['node_modules/**']
-			})
+			commonjs() // so Rollup can convert `ms` to an ES module
 		]
 	},
 
@@ -31,13 +26,8 @@ export default [
 	{
 		input: 'src/js/index.js',
 		output: [
-			{ file: pkg.main, exports: 'named', name: 'Toasteo', format: 'cjs' },
-			{ file: pkg.module, exports: 'named', name: 'Toasteo', format: 'es' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**']
-			})
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
 		]
 	}
 ];
